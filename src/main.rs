b32aae999274234f8a9c5a858c46a39482e103c1
B.Billion <buhebillion@gmail.com>
2025-03-09 17:40:53 +0200

    trivial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+fn
+main
+()
+{
+    let mut x: u128 = 7;
+    println!("Der Wert von x in main() ist: {x}");
+    x = 5;
+    println!("Der Wert von x in main() ist: {x}");
+
+    demo()
+}
+
+fn
+demo
+()
+{
+    let x = 11;
+    let x = x + 2;
+
+    {
+        let x = (x * 2) - 3;
+        println!("Der Wert von x im inneren Gültigkeitsbereich ist: {x}");
+    }
+
+    println!("Der Wert von x ist: {x}");
 }
